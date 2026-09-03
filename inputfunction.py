bec84ee9712d37ba4bdaf22340436a3c5429d9de
Md Shakil Hossain Saikat <mohammadsaikat2023@gmail.com>
2026-09-03 15:13:52 +0600

    input function (taking input from user)

---
+++ inputfunction.py
@@ -0,0 +1,15 @@
+# taking user input
+
+a = int(input("enter a number: "))
+b = int(input("enter another number: "))
+print("a+b = ", a+b)
+
+print(type(a))
+
+c = input("enter any number: ")
+print("the number is: ", c)
+print(type(c))
+
+d = float(input("enter another number again: "))
+print("the number is: ",d)
+print(type(d))
