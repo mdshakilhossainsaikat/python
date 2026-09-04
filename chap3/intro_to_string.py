ddf87ec1cf23393d134ea16fb5ccda0f66f0fb7f
Md Shakil Hossain Saikat <mohammadsaikat2023@gmail.com>
2026-09-04 20:29:37 +0600

    stringintro

---
+++ chap3/intro_to_string.py
@@ -0,0 +1,21 @@
+name1 = 'shakil'
+name2 = "hossain"
+name3 = '''saikat'''
+
+print(name1)
+print(name2)
+print(name3)
+
+print("length of name 1 is: ",len(name1))
+
+shortname = name1[0:3]
+print(shortname)
+print(name1[3:])
+print(name1[:3])
+print(name1[:])
+
+# skip value
+
+alphabate = "abcdefghijklmnopqrstuvwxyz"
+
+print(alphabate[3:20:3])
